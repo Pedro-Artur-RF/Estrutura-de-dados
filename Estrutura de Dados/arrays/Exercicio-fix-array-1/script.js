e2d7c001f7e9bc6c84781ba34227d69a3961b1b5
Pedro Artur (Motsu) <71746213+N0-N4M3-B0Y@users.noreply.github.com>
2023-03-27 12:46:14 -0300

    Updated code (WIP)

--- Estrutura de Dados/arrays/Exercicio-fix-array-1/script.js
+++ Estrutura de Dados/arrays/Exercicio-fix-array-1/script.js
@@ -1,22 +1,17 @@
-let array = ['M4-A1', 'AK-47', 'M95'];
-let text = document.getElementById('input');
-let addBtn = document.getElementById('addBtn');
-let orderBtn = document.getElementById('orderBtn');
+let array = ["M4-A1", "AK-47", "M95"];
+let text = document.getElementById("input");
+let addBtn = document.getElementById("addBtn");
+let orderBtn = document.getElementById("orderBtn");
 
-addBtn.addEventListener('click', () => {
-        if (text.value === ' ' ) {
-            array.push(text.value);
-            console.warn(array);
-        }
-        else (
-            console.log('Informe um valor válido')
-        )
-        
-    });
+addBtn.addEventListener("click",  () => {
+const inputText = text.value
+  if (text === "") {
+    array.push(inputText);
+    console.warn(array);
+  } else console.log("Informe um valor válido");
+});
 
-    orderBtn.addEventListener('click', () => {
-        console.log(array)
-        array.sort();
-        console.warn(array);
-    })
-
+orderBtn.addEventListener("click", () => {
+  array.sort();
+  console.warn(array);
+});
